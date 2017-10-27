454d101127ff01bbe593f936e6568c7d53ae3e55
yyyura <yyyura@gmail.com>
2017-10-27 20:11:13 +0100

    lab 6 Bonus Question

--- wk6/instance-check.sh
+++ wk6/instance-check.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#aws ec2 describe-instance-status --instance-ids i-033390d4b2e4fb049
-#aws ec2 describe-instance-status --instance-ids i-07bc96b8192810fe0
-
-
 value=`aws ec2 describe-instance-status --instance-ids $1 | grep running |  wc -l`
 if [ $value -lt 1 ]; then
 echo "no such instance running"
@@ -10,5 +6,6 @@ else
 echo "the instance is running"
 fi
 
-#if [ $value -le $1 ]; then
-#aws ec2 describe-instance-status --instance-ids i-033390d4b2e4fb049 | grep running |  wc -l
+#crontab -e
+#*/1 * * * * /home/epa/epa-comp/wk6/instance-check.sh i-033390d4b2e4fb049 >> /home/epa/epa-comp/wk6/script_output.log
+#*/1 * * * * /home/epa/epa-comp/wk6/instance-check.sh i-07bc96b8192810fe0 >> /home/epa/epa-comp/wk6/script_output.log
