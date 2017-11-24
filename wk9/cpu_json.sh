3465c6a6726f4dfef6610febef62fe3e9b8eced7
yyyura <yyyura@gmail.com>
2017-11-24 19:07:50 +0000

    lab 9

--- wk9/cpu_json.sh
+++ wk9/cpu_json.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mpstat -o JSON
+#mpstat -o JSON
 #interval of 10 seconds and produce 1 sample of CPU data
-mpstat 10 1
+#mpstat 10 1
+mpstat 10 1 -o JSON | jq '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'
